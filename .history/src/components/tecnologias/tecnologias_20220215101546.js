5b4b2ccda63f76d3ffb31c480732296f58ec1864
renan-oliveira93 <84824361+renan-oliveira93@users.noreply.github.com>
2022-02-15 10:18:18 -0300

    updateerr

--- .history/src/components/tecnologias/tecnologias_20220215101546.js
+++ .history/src/components/tecnologias/tecnologias_20220215101546.js
@@ -9,24 +9,24 @@ import styles from '../../../styles/Home.module.css';
                    <h3>javascript</h3>
                    < img className={styles.icone} src='/assets/javascript.png' alt='icone Javascrip'/>
                </div>
-               <div>
+               <div className={styles.icones_tecnologias}>
                    <h3>React</h3>
                    < img className={styles.icone} src='/assets/react-link.png' alt='icone React'/>
                </div>
-               <div>
+               <div className={styles.icones_tecnologias}>
                     <h3>React Native</h3>
                     < img className={styles.icone} src='/assets/react-native.png' alt='icone React Native'/>
                </div>
-               <div>
+               <div className={styles.icones_tecnologias}>
                    <h3>Vue JS</h3>
                    < img className={styles.icone} src='/assets/vue.png' alt='icone Vue JS'/>
                </div>
                
-               <div>
+               <div className={styles.icones_tecnologias}>
                    <h3>Node JS</h3>
                    < img className={styles.icone} src='/assets/node.png' alt='icone Node JS'/>
                </div>                              
-               <div>
+               <div className={styles.icones_tecnologias}>
                    <h3>Bootstrap</h3>
                    < img className={styles.icone} src='/assets/bootstrap.png' alt='icone Bootstrap'/>
                </div>               
