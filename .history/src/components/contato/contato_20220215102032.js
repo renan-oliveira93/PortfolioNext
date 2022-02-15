03e7eecc7c68759eb6b5680694843bb9af3950cb
renan-oliveira93 <84824361+renan-oliveira93@users.noreply.github.com>
2022-02-15 10:22:07 -0300

    updateerr2

--- .history/src/components/contato/contato_20220215102032.js
+++ .history/src/components/contato/contato_20220215102032.js
@@ -13,13 +13,13 @@ function Contato () {
                     <a href="https://github.com/renan-oliveira93" target="_blank" rel="noreferrer">
                     < img src='/assets/github.png' width='30' height='30'  alt="icone GitHub"/></a>
                     <a href="mailto:renan_oliveira93@outlook.com.br" target="_blank" rel="noreferrer">
-                    < img  src='/assets/e-mail.png' width='30' height='30' alt="icone e-mail"/></a>                                
+                    < img className="container-contato-item " src='/assets/e-mail.png' width='30' height='30' alt="icone e-mail"/></a>                                
             </div>   
             <div className={styles.container_contato_itens}>
                     <a href="https://api.whatsapp.com/send?phone=5511964885816" target="_blank" rel="noreferrer">
-                    < img  src='/assets/whats-app.png' width='30' height='30' width='30' height='30' alt="icone whats-app"/></a>
+                    < img className="container-contato-item " src='/assets/whats-app.png' width='30' height='30' width='30' height='30' alt="icone whats-app"/></a>
                     <a href="tel:11964885816" target="_blank" rel="noreferrer">
-                    < img  src='/assets/telefone.png'width='30' height='30' alt="icone telefone"/></a>  
+                    < img className="container-contato-item " src='/assets/telefone.png'width='30' height='30' alt="icone telefone"/></a>  
             </div>         
             
         </div>
--- .history/src/components/contato/contato_20220215102045.js
+++ .history/src/components/contato/contato_20220215102045.js
@@ -17,9 +17,9 @@ function Contato () {
             </div>   
             <div className={styles.container_contato_itens}>
                     <a href="https://api.whatsapp.com/send?phone=5511964885816" target="_blank" rel="noreferrer">
-                    < img  src='/assets/whats-app.png' width='30' height='30' width='30' height='30' alt="icone whats-app"/></a>
+                    < img className="container-contato-item " src='/assets/whats-app.png' width='30' height='30' width='30' height='30' alt="icone whats-app"/></a>
                     <a href="tel:11964885816" target="_blank" rel="noreferrer">
-                    < img  src='/assets/telefone.png'width='30' height='30' alt="icone telefone"/></a>  
+                    < img className="container-contato-item " src='/assets/telefone.png'width='30' height='30' alt="icone telefone"/></a>  
             </div>         
             
         </div>
