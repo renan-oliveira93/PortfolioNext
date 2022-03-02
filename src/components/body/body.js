cc3c3b6149d0b7d329b4aba91d38171a9d1527fa
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2022-03-02 08:34:54 -0300

    teste

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -3,21 +3,20 @@ import Apresentacao from '../apresentacao/apresentacao';
 import Bio from '../bio/bio';
 import Tecnologias from '../tecnologias/tecnologias';
 import Contato from '../contato/contato';
-import Tempo from '../tempo/tempo';
 import styles from '../../../styles/Home.module.css';
 
- function Body () {
-   return(
-     <div >          
-               <Apresentacao/>
-               <NavBar/> 
-          <div className={styles.div_1}>
-               <Bio/>
-               <Contato/>
-          </div> 
-               <Tecnologias/>
-                             
-     </div>
-    )
+function Body() {
+     return (
+          <div >
+               <Apresentacao />
+               <NavBar />
+               <div className={styles.div_1}>
+                    <Bio />
+                    <Contato />
+               </div>
+               <Tecnologias />
+
+          </div>
+     )
 };
 export default Body;
